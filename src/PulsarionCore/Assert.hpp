cb4c959c3e9191391433700e42e3a2cac82429a9
9636 Dev <9636dev@gmail.com>
2024-01-28 22:06:08 +0100

    Refactored assert to be more readable and added cpp.hint

--- src/PulsarionCore/Assert.hpp
+++ src/PulsarionCore/Assert.hpp
@@ -4,14 +4,17 @@
 #include "Log.hpp" // Required for asserts
 
 #ifdef PULSARION_DEBUG
-#define PULSARION_INTERNAL_ASSERT_WITH_MSG(x, msg, ...) \
-    if (!(x)) { \
-        PULSARION_CORE_LOG_ERROR("Assertion Failed at " __FILE__ ":{0}: {1}", __LINE__, msg); \
-        PULSARION_DEBUG_BREAK(); \
+#define PULSARION_ASSERT(x, msg) \
+    { \
+        auto condition = (x); \
+        if (!(condition)) { \
+            PULSARION_CORE_LOG_ERROR("Assertion Failed at " __FILE__ ":{0}: {1}", __LINE__, msg); \
+            PULSARION_DEBUG_BREAK(); \
+        } \
     }
-#define PULSARION_ASSERT(...) PULSARION_INTERNAL_ASSERT_WITH_MSG(__VA_ARGS__, "No message provided")
 #define PULSARION_RUN_IF_DEBUG(...) do { __VA_ARGS__; } while (false)
 #else
-#define PULSARION_ASSERT(x, ...) ((void)0)
+#define PULSARION_ASSERT(x, ...) { (void)(x); } 
+// We do this so that functions with [[nodiscard]] wont warn 
 #define PULSARION_RUN_IF_DEBUG(...) ((void)0)
 #endif
